6973877839aea8b6ba1cc3c60b4c47091722dc6a
theetha1608 <theerthakunjusss@gmail.com>
2024-05-04 15:54:11 +0530

    navbar navigation to user profile

--- src/app/Components/side-navbar/side-navbar.component.ts
+++ src/app/Components/side-navbar/side-navbar.component.ts
@@ -1,37 +1,63 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../../Services/user.service';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, RouterLink, MatIconModule, SideNavbarComponent],
   templateUrl: './side-navbar.component.html',
   styleUrl: './side-navbar.component.scss'
 })
 export class SideNavbarComponent {
   isSideOut = true;
-  constructor(private router: Router){}
+  userEmail: string = '';
+  userProfileImage: string = '';
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) {
+    // Fetch user details when the component initializes
+    this.fetchUserDetails();
+  }
 
   toggleSideOut(): void {
     this.isSideOut = !this.isSideOut;
   }
 
-    onHome(){
+  onHome(): void {
     this.router.navigate(['/dashboard']);
-    } 
-    onProfile(){
+  } 
+
+  onProfile(): void {
     this.router.navigate(['/user-profile']);
-    }
-    onAddTask(){
+  }
+
+  onAddTask(): void {
     this.router.navigate(['/create-task']);
-    }
-    onLogout(){
+  }
+
+  onLogout(): void {
     this.router.navigate(['']);
+  }
+
+  private fetchUserDetails(): void {
+    const userId = this.userService.getCurrentUserId();
+    if (userId) {
+      this.userService.getUserById(userId).subscribe(
+        user => {
+          this.userEmail = user.email;
+          this.userProfileImage = `assets/Images/${user.profilePicture}`;
+        },
+        error => {
+          console.error('Error fetching user details:', error);
+        }
+      );
+    } else {
+      console.error('User ID is null.');
     }
-    
+  }
 }
-    
-
-
